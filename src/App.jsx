a72703f93c87abe8655adc1dbd03514a35678bee
mag-garcia <mag.garcia.felice@gmail.com>
2023-03-13 17:06:15 -0300

    first react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,87 @@
+import React, { useState } from "react";
+import Card from "./Card";
+import style from "./App.module.css";
 
-//Aqui deberias escribir tus funciones de validacion
+const App = () => {
+  const [userData, setUserData] = useState({
+    username: "",
+    password: "",
+  });
 
+  const [errors, setErrors] = useState({
+    error: "",
+    errorValidation: true,
+  });
 
-function App() {
-  //Aqui deberias agregar los estados y los handlers para los inputs
+  const validations = () => {
+    if (
+      userData.username[0] === "" ||
+      userData.username.length <= 3 ||
+      userData.password.length < 6
+    ) {
+      setErrors({
+        error: "Por favor chequea que la información sea correcta",
+        errorValidation: true,
+      });
+    } else
+      setErrors({
+        error: "",
+        errorValidation: false,
+      });
+  };
 
+  const handleInput = (event) => {
+    const property = event.target.name;
+    const value = event.target.value;
+    setUserData({
+      ...userData,
+      [property]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    validations();
+  };
+  console.log(userData);
 
   return (
-    <div className="App">
-     <h1>Carga de estudiantes</h1>
-     <form>
-      {/* aqui deberias comenzar a escribir tu codigo */}
-     </form>
-    </div>
+    <>
+      <div className={style.Wrapper}>
+        {!errors.errorValidation && <Card userData={userData} />}
+
+        <form className={style.Form} onSubmit={handleSubmit}>
+          <div className={style.formWrapper}>
+            <div className={style.inputWrapper}>
+              <input
+                className={style.Input}
+                name="username"
+                value={userData.username}
+                autoComplete="off"
+                placeholder="Username"
+                type="text"
+                onChange={handleInput}
+              ></input>
+
+              <input
+                className={style.Input}
+                value={userData.password}
+                autoComplete="off"
+                name="password"
+                placeholder="Password"
+                type="password"
+                onChange={handleInput}
+              ></input>
+              <span className={style.Span}>{errors.error}</span>
+              <button className={style.Button} type="submit">
+                Login
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import style from './Card.module.css'
 
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario 
 
-const Card = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Card = ({userData}) => {
+ return(
+  <div className={style.Wrapper}>
+    <h1 >Bienvenido {userData.username}!!!</h1>
+    <h2 >La contraseña que ingresaste tiene:  {userData.password.length} caracteres!!!</h2>
+  </div>
+ )
 }
 
 export default Card
